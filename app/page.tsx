4e86306a870d87b9a5644eea272c4a030da722d3
NipunBasnayake <nipunsathsara1999@gmail.com>
2025-05-12 11:45:07 +0530

    add global.css

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">
+        Hello World
+      </h1>
+      <button className="btn btn-secondary">
+        click me
+      </button>
+    </div>
+  )
+}
